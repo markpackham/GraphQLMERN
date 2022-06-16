cb409305d51abbc178d989519f1baa8a9f19e61d
mark <markpackham1@gmail.com>
2022-06-16 09:54:32 +0100

    add Client and Project models
    https://youtu.be/BcLNfwF04Kw?t=2953

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
 
+  // we use the colors package to get the "cyan" text in the terminal so it's easier to read
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
+
+// What my local database looks like in my .env
+// MONGO_URI = 'mongodb://localhost:27017/graphql_mern'
