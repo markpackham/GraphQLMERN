c82339957cfb875d71ae835e8a075e5b4349784b
mark <markpackham1@gmail.com>
2022-06-17 16:53:36 +0100

    add Apollo Provider
    https://youtu.be/BcLNfwF04Kw?t=5289

--- clientReact/src/App.js
+++ clientReact/src/App.js
@@ -1,11 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Header from "./components/Header";
+import NotFound from "./pages/NotFound";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="container">
-      <h1>Test</h1>
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Test</h1>
+        </div>
+      </ApolloProvider>
+    </>
   );
 }
 
