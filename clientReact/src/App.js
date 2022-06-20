9bb47d904f1618a19eacd558e0aef8f1d17d16b2
mark <markpackham1@gmail.com>
2022-06-20 11:21:11 +0100

    add Client Modal
    https://youtu.be/BcLNfwF04Kw?t=6850

--- clientReact/src/App.js
+++ clientReact/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Header from "./components/Header";
-import Clients from "./components/Clients";
+
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 const cache = new InMemoryCache({
@@ -32,10 +33,15 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
