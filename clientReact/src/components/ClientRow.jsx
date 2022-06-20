d77d4dede6c6e0948b179e0461d23a4fc796cdcc
mark <markpackham1@gmail.com>
2022-06-20 17:24:47 +0100

    finish off delete cascade between a removed client and their projects
    https://youtu.be/BcLNfwF04Kw?t=11601

--- clientReact/src/components/ClientRow.jsx
+++ clientReact/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({client}) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {variables: {id: client.id},
@@ -16,6 +17,7 @@ export default function ClientRow({client}) {
         },
       });
     },
+    refetchQueries: [{query: GET_PROJECTS}]
   });
 
   return (
