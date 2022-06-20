b6750ebec3cf36c6b581ea907f873ba01da24049
mark <markpackham1@gmail.com>
2022-06-20 12:24:41 +0100

    add project queries
    https://youtu.be/BcLNfwF04Kw?t=7680

--- clientReact/src/components/Projects.jsx
+++ clientReact/src/components/Projects.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import Spinner from './Spinner';
+import { useQuery } from '@apollo/client';
+import ProjectCard from './ProjectCard';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function Projects() {
+
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
+
   return (
     <div>Projects</div>
   )
